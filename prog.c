35f13a8fb1017a8a20853efa438ba35d351d3c2e
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2022-04-23 22:39:06 +0530

    Add files via upload

---
+++ prog.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+int main() 
+{ 
+    long int arr[10]; 
+    long int sum,product,i;
+    /*To Read array elements*/
+    printf("\nEnter elements : \n"); 
+    for(i=0; i<10; i++) 
+    { 
+        printf("Enter arr[%d] : ",i); 
+        scanf("%d",&arr[i]); 
+    } 
+    /*To calculate sum and product*/
+    sum=0;
+    product=1;
+    for(i=0; i<10; i++)
+    {
+        sum=sum+arr[i];
+        product=product*arr[i];
+    }
+    printf("\nSum of array is     : %d"  ,sum); 
+    printf("\nProduct of array is : %d\n",product); 
+
+    return 0;
+}
